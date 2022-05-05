d851e767be08454d3c01fad485b1bc9d6aaf4c68
Brina <sabrinazanin97@gmail.com>
2022-05-04 22:28:12 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 
 import React, { useState } from "react";
-import PageSelection from "./components/PageSelection";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+//import PageSelection from "./components/PageSelection";
 
 function App() {
   const [selections] = useState([
@@ -20,12 +28,12 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <main>
-        <About></About>
-        <Projects></Projects>
-        <Contact></Contact>
-        <Resume></Resume>
-      </main>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+      </Switch>
       <Footer></Footer>
     </div>
   );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,54 +7,59 @@ import laptop from "../../assests/images/laptop.png";
 
 function Projects() {
   return (
-    <section id="my-work" class="bio">
+    <section id="my-work" className="bio">
       <h2 class="title">My Work</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <a
+              href="https://zsvoboda87.github.io/Bored-AF/"
+              className="main-img"
+            >
+              <img
+                src={boredAf}
+                alt="brand of the website with the words Bored a f and names of the developers"
+              />
+              <div class="bottom-left">BoredAF Javascript</div>
+            </a>
+          </div>
+          <div className="col">
+            <a
+              href="https://pure-everglades-43557.herokuapp.com/"
+              className="image1"
+            >
+              <img src={desk} alt="laptop on a desk in a bright room" />
+              <div class="bottom-left">CC Javascript</div>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://brinabean.github.io/run-buddy/" className="image1">
+              <img src={running} alt="person tying their shoe ready to run" />
+              <div class="bottom-left">Run Buddy html</div>
+            </a>
+          </div>
+        </div>
+      </div>
 
-      <div>
-        <a
-          href="https://zsvoboda87.github.io/Bored-AF/"
-          target="_blank"
-          class="main-img"
-        >
-          <img
-            src={boredAf}
-            alt="brand of the website with the words Bored a f and names of the developers"
-          />
-          <div class="bottom-left">BoredAF Javascript</div>
-        </a>
-
-        <a
-          href="https://pure-everglades-43557.herokuapp.com/"
-          target="_blank"
-          class="image1"
-        >
-          <img src={desk} alt="laptop on a desk in a bright room" />
-          <div class="bottom-left">CC Javascript</div>
-        </a>
-        <a
-          href="https://brinabean.github.io/run-buddy/"
-          target="_blank"
-          class="image1"
-        >
-          <img src={running} alt="person tying their shoe ready to run" />
-          <div class="bottom-left">Run Buddy html</div>
-        </a>
-        <a
-          href="https://brinabean.github.io/Robot-Gladiators/"
-          target="_blank"
-          class="image2"
-        >
-          <img src={robot} alt="Cute robot ready for battle" />
-          <div class="bottom-left">Robot Battle javascript</div>
-        </a>
-        <a
-          href="https://brinabean.github.io/Horiseon-refactored/"
-          target="_blank"
-          class="image3"
-        >
-          <img src={laptop} alt="laptop with tree graph on the screen" />
-          <div class="bottom-left">Horiseon html</div>
-        </a>
+      <div className="row">
+        <div className="col">
+          <a
+            href="https://brinabean.github.io/Robot-Gladiators/"
+            class="image2"
+          >
+            <img src={robot} alt="Cute robot ready for battle" />
+            <div class="bottom-left">Robot Battle javascript</div>
+          </a>
+        </div>
+        <div className="col">
+          <a
+            href="https://brinabean.github.io/Horiseon-refactored/"
+            class="image3"
+          >
+            <img src={laptop} alt="laptop with tree graph on the screen" />
+            <div class="bottom-left">Horiseon html</div>
+          </a>
+        </div>
       </div>
     </section>
   );
